a09a2c3a18f0622e8b8f646972c08649b9643332
pacrob <paul@pacrob.com>
2023-04-03 16:29:13 -0600

    test lint with all py versions

--- setup.py
+++ setup.py
@@ -79,6 +79,5 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: Implementation :: PyPy",
     ],
 )
