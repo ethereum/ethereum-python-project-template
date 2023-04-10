ecacde34927a73a71fa2f69dd314fb3850f375c9
pacrob <paul@pacrob.com>
2023-04-10 13:45:02 -0600

    repin flake8 and misc updates

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ extras_require = {
         "pytest-xdist>=2.4.0",
     ],
     "lint": [
-        "flake8>=5.0.0",
-        "flake8-bugbear>=22.0.0",
+        "flake8==6.0.0",
+        "flake8-bugbear==23.3.23",
         "isort>=5.10.1",
         "mypy==0.971",
         "pydocstyle>=5.0.0",
